51d34a763f1342dc72cdabd1ca9eb1b5ee4c2c54
milanpanyik <milanpanyik@gmail.com>
2024-05-14 15:42:06 +0200

    setting up videos displaying

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { Box, Stack, Typography } from "@mui/material";
 const ExerciseVideos = ({ exerciseVideos, name }) => {
+  console.log(exerciseVideos);
 
-    console.log(exerciseVideos);
-
-    if(!exerciseVideos.length) return 'loading...';
+  if (!exerciseVideos.length) return "loading...";
   return (
     <Box sx={{ marginTop: { lg: "200px", xs: "20px" } }} p="20px">
       <Typography variant="h3" mb="33px">
@@ -14,22 +13,26 @@ const ExerciseVideos = ({ exerciseVideos, name }) => {
         </span>
         !
       </Typography>
-      <Stack justifyContent="flex-start" flexWrap="wrap" alignItems="center" sx={{
-        flexDirection: {lg: 'row'},
-        gap: {lg:'110', xs: '0'}
-      }}>
-        {exerciseVideos?.slice(0, 3).map((item, index)=>{
-             <a
-             key={index}
-             className="exercise-video"
-             href={`https://www.youtube.com/watch?v=${item.video.videoId}`}
-             target="_blank"
-             rel="noreferrer"
-           >
-                <img src={item.video.thumbnails[0].url} alt={item.video.title}/>
-            </a>
-        })}
-
+      <Stack
+        justifyContent="flex-start"
+        flexWrap="wrap"
+        alignItems="center"
+        sx={{
+          flexDirection: { lg: "row" },
+          gap: { lg: "110", xs: "0" },
+        }}
+      >
+        {exerciseVideos?.slice(0, 3).map((item, index) => ( 
+          <a
+            key={index}
+            className="exercise-video"
+            href={`https://www.youtube.com/watch?v=${item.video.videoId}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={item.video.thumbnails[0].url} alt={item.video.title} />
+          </a>
+        ))}
       </Stack>
     </Box>
   );
